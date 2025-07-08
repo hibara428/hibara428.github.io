25b52d50c923f7410725f8941141d8ffbce7f51b
hibara428 <hibara428@gmail.com>
2025-07-09 08:34:02 +0900

    Fix url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,32 +1,32 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'hibara428.github.io',
-  tagline: 'hibara428',
-  favicon: 'img/favicon.ico',
-  url: 'https://hibara428.github.io',
-  baseUrl: '/',
-  organizationName: 'hibara428',
-  projectName: 'hibara428.github.io',
-  deploymentBranch: 'gh-pages',
+  title: "hibara428.github.io",
+  tagline: "hibara428",
+  favicon: "img/favicon.ico",
+  url: "https://hibara428.github.io",
+  baseUrl: "/",
+  organizationName: "hibara428",
+  projectName: "hibara428.github.io",
+  deploymentBranch: "gh-pages",
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   i18n: {
-    defaultLocale: 'ja',
-    locales: ['ja'],
+    defaultLocale: "ja",
+    locales: ["ja"],
   },
 
   plugins: [
     [
-      '@docusaurus/plugin-google-gtag',
+      "@docusaurus/plugin-google-gtag",
       {
-        trackingID: 'G-4DQ4RV5MJG',
+        trackingID: "G-4DQ4RV5MJG",
         anonymizeIP: true,
       },
     ],
@@ -34,16 +34,16 @@ const config: Config = {
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           editUrl:
-            'https://github.com/hibara428/hibara428.github.io/tree/main/',
+            "https://github.com/hibara428/hibara428.github.io/tree/main/",
         },
         blog: false,
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -51,33 +51,33 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
       hideOnScroll: true,
-      title: 'hibara428.github.io',
+      title: "hibara428.github.io",
       items: [],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Social',
+          title: "Social",
           items: [
             {
-              label: 'GitHub',
-              href: 'https://github.com/hibara428',
+              label: "GitHub",
+              href: "https://github.com/hibara428",
             },
             {
-              label: 'note',
-              href: 'https://note.com/hibara428/',
+              label: "note",
+              href: "https://note.com/hibara428/",
             },
             {
-              label: 'Zenn',
-              href: 'https://zenn.dev/hibara428',
+              label: "Zenn",
+              href: "https://zenn.dev/hibara428",
             },
             {
-              label: 'X',
-              href: 'https://x.com/hiba',
+              label: "X",
+              href: "https://x.com/hibara428",
             },
           ],
         },
@@ -85,7 +85,7 @@ const config: Config = {
       copyright: `Copyright © ${new Date().getFullYear()} hibara428. Built with Docusaurus.`,
     },
     colorMode: {
-      defaultMode: 'dark',
+      defaultMode: "dark",
       disableSwitch: false,
       respectPrefersColorScheme: true,
     },
